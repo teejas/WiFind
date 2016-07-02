5e205b66c303eccb419d3dafcd9f352c95d0b9f5
Tejas Siripurapu <tejassiripurapu@Mac-Daddy.local>
2016-07-01 20:33:12 -0700

    moved data from location-list to locationCtrlr

--- app_server/controllers/location.js
+++ app_server/controllers/location.js
@@ -1,6 +1,33 @@
 // GET home page
 module.exports.home = function(req, res) {
-    res.render('location-list', {title: 'WiFind'});
+  res.render('location-list', {
+    title: 'WiFind - find a place to get connected quick!',
+    pageHeader: {
+      title: 'WiFind',
+      subTitle: 'Find places to work with wifi near you!'
+    },
+    locations: [{
+      name: 'Starbucks',
+      address: '125 Wallaby Lane, Sydney, Australia',
+      rating: 4,
+      facilities: ['Hot Drinks', 'Food', 'Free WiFi'],
+      distance: '100mi'
+    },
+    {
+      name: 'Caffeinator',
+      address: '15225 Blue Gum Court, Saratoga, CA',
+      rating: 5,
+      facilities: ['Food', 'Free WiFi'],
+      distance: '500mi'
+    },
+    {
+      name: 'WiFood',
+      address: '19489 Brockton Lane, Saratoga, CA',
+      rating: 3,
+      facilities: ['Hot Drinks', 'Food', 'Free WiFi'],
+      distance: '50mi'
+    }]   
+  });
 };
 
 // GET location details page
