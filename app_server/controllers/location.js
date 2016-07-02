83be6a75ddfa8db612a105e5678cb5b2245fcd68
Tejas Siripurapu <tejassiripurapu@Mac-Daddy.local>
2016-07-02 11:27:34 -0700

    moved data from views to controllers

--- app_server/controllers/location.js
+++ app_server/controllers/location.js
@@ -2,6 +2,7 @@
 module.exports.home = function(req, res) {
   res.render('location-list', {
     title: 'WiFind - find a place to get connected quick!',
+    sidebar: 'Ever feel the need to get connected immediately? WiFind helps you find the next hotspot to sit down, grab some food or a drink, and get connected immediately!',
     pageHeader: {
       title: 'WiFind',
       subTitle: 'Find places to work with wifi near you!'
@@ -32,10 +33,59 @@ module.exports.home = function(req, res) {
 
 // GET location details page
 module.exports.locationDetails = function(req, res) {
-    res.render('location-info', {title: 'Location Info'});
+    res.render('location-info', {
+      title: 'Starbucks',
+      sidebar: {
+        main: 'Starbucks is on WiFind because it has great wifi and a place to sit down to get work done.',
+        sub: 'If you\'ve been here and loved it or hated it, please write a helpful review for others like you.'
+      },
+      location: {
+        rating: 4,
+        addres: '125 Wallaby Lane, Sydney, Australia',
+        facilities: ['Hot Drinks', 'Food', 'Free WiFi'],
+        distance: '100mi',
+        coords: { lat: 51.455041, lng: -0.9690884},
+        openHours: [
+          { 
+            days: 'Monday-Friday',
+            open: '7:00am',
+            close: '7:00pm',
+            closed: false
+          },
+          {
+            days: 'Saturday',
+            open: '8:00am',
+            close: '5:00pm',
+            closed: false
+          },
+          {
+            days: 'Sunday',
+            closed: true
+          }
+        ],
+        reviews: [
+          {
+            rating: 4,
+            author: 'Tanuj Siripurapu',
+            timestamp: 'July 1, 2016',
+            review: 'Such an amazing place I love their Earl Grey Chai!'
+          },
+          {
+            rating: 3,
+            author: 'Busy Cooker',
+            timestamp: 'June 30, 2015',
+            review: 'Wifi is all I wanted and they had it.'
+          }
+        ]
+      }
+    });
 };
 
 // GET make a review page
 module.exports.review = function(req, res) {
-    res.render('location-review-form', {title: 'Add Review'});
+    res.render('location-review-form', {
+      title: 'Add Review for Starbucks',
+      pageHeader: 'Review Starbucks',
+      
+    });
 };
